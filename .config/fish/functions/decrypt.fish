9f048ac50c87bb588cdb3245c8dbb4ff1365e39e
Yettimania <kylesnyder42@gmail.com>
2024-01-19 17:05:48 -0800

    fish: edit encrypt/decyrpt to handle folders with compression

--- .config/fish/functions/decrypt.fish
+++ .config/fish/functions/decrypt.fish
@@ -1,4 +1,5 @@
 function decrypt --description 'Decrypt file'
     set output $(path change-extension '' $argv[1])
+
     command gpg --output $output --decrypt $argv[1]
 end
--- .config/fish/functions/encrypt.fish
+++ .config/fish/functions/encrypt.fish
@@ -1,5 +1,32 @@
 function encrypt --description 'Encrypt file'
-    set f $(path change-extension '' $argv[1])
+    if test -f $argv[1]
+        set input $argv[1]
+        set isFile true
+        set f $(path change-extension '' $argv[1])
+    else
+        set tar_output $argv[1].tar.gz
+        tar -zcf $tar_output $argv[1]
+        set input $tar_output
+        set isFile false
+        set f $tar_output 
+    end
+
     set recipient $(echo $PERSONAL_EMAIL)
-    command gpg --output $f.gpg --encrypt --recipient $recipient $argv[1]
+    command gpg --output $f.gpg --encrypt --recipient $recipient $input
+    while true
+
+        read -l -P 'Do you want to delete input? [y/N] ' confirm
+
+        switch $confirm
+          case Y y
+            if test isFile = true
+                rm $argv[1]
+            else
+                rm -rf $argv[1]
+            end
+            return 0
+          case '' N n
+            return 1
+        end
+    end
 end
