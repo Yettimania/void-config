73382e1547588ace8cc09282235241af1da7fc7f
Yettimania <kylesnyder42@gmail.com>
2024-06-14 08:11:10 -0700

    fish: added aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,10 +16,26 @@ direnv hook fish | source
 set -gx PATH $HOME/.local/bin $HOME/.cargo/bin $PATH
 
 set -gx GPG_TTY $(tty)
-set -gx EDITOR hx
+set -gx EDITOR vim
 set -gx READER zathura
 set -gx TERMINAL alacritty
 set -gx BROWSER firefox
 set -gx VIEWER sxiv
 set -gx FILE fd
 set -gx TOUCH_STATUS 1
+
+# Git Aliases
+alias gnb='git checkout -b' # new branch
+alias ga='git add . --all'
+alias gb='git branch'
+alias gc='git clone'
+alias gci='git commit -a'
+alias gcm='git commit -m'
+alias gco='git checkout'
+alias gdf='git diff' # git diff (full)
+alias gi='git init'
+alias gl='git log'
+alias gs='git status'
+alias gsw='git status'
+alias gt='git tag'
+alias gu='git pull' # gu = git update
